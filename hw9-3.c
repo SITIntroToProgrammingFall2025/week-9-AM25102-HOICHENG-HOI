3b1bf0b7318dd1078ef5e8b7d3b467d200e54ea0
AM25102-HOICHENG-HOI <am25102@shibaura-it.ac.jp>
2025-12-03 14:46:56 +0900

    Add files via upload

--- hw9-3.c
+++ hw9-3.c
@@ -40,16 +40,19 @@ int main()
 	printf("The multiplication product of matrix A and matrix B: \n");
 
 
-	for(j=0; j<2; j++)  
+	for(i=0; i<2; i++)
 	{
+		for(j=0; j<2; j++)  
+		{
 		c[i][j] = 0;
 		for(k=0; k<3; k++)
-            {
+          	  {
                 c[i][j] += a[i][k] * b[k][j];
-            }
+          	  }
             
-            printf("%lf ", c[i][j]);
-        }
+            printf("%.1g ", c[i][j]);
+      	  }
+	}
         printf("\n");
 	return 0;
 }
