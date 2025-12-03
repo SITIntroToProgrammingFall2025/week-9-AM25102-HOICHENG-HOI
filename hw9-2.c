915656b63e699d29c107963388c462fc4dd641d3
AM25102-HOICHENG-HOI <am25102@shibaura-it.ac.jp>
2025-12-03 13:01:32 +0900

    Add files via upload

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main() 
+{
+	int a[3][3];
+	int i, j;
+	double d;
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<3; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}	
+	printf("You entered\n");
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<3; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+	}
+	printf("Output\n");
+	for(i=0; i<3; i++)
+	{
+		for(j=2; j>=0; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+	}
+
+	return 0;
+}
