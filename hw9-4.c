317b550be861884f3169ed3c60abfb706b25b460
AM25102-HOICHENG-HOI <am25102@shibaura-it.ac.jp>
2025-12-05 17:27:44 +0900

    Add files via upload

--- hw9-4.c
+++ hw9-4.c
@@ -2,8 +2,9 @@
 
 int main() {
 	int r, g, b;
-	printf("Enter RGB values (0-255): ");
-	scanf("%d %d %d", &r, &g, &b);
+	scanf("%d", &r);
+	scanf("%d", &g);
+	scanf("%d", &b);
 
 	printf("#%02X%02X%02X\n", r, g, b);
 
