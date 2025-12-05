18a8c93fad355c7cad627785026e5640d9fc7812
AM25102-HOICHENG-HOI <am25102@shibaura-it.ac.jp>
2025-12-05 17:43:56 +0900

    Add files via upload

--- ahw9-1.c
+++ ahw9-1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() 
+{
+	int r, g, b, x=0, y=0, z=0;
+	scanf("%d", &r);
+	scanf("%d", &g);
+	scanf("%d", &b);
+
+	if(r>127){x=1;}
+	if(g>127){y=1;}
+	if(b>127){z=1;}
+
+	if(x==0&&y==0&&z==0){printf("The nearest color is Black\n");
+	if(x==1&&y==0&&z==0){printf("The nearest color is Red\n");
+	if(x==0&&y==1&&z==0){printf("The nearest color is Black\n");
+	if(x==0&&y==0&&z==1){printf("The nearest color is Black\n");
+	else{printf("The nearest color is Black\n");
+
+	return 0;
+}
